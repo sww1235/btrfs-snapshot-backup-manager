fc44e378b0a2b90c58735cc0d1680bb8d83c73df
sww1235 <sww1235@gmail.com>
2019-01-26 18:42:19 -0700

    added mutually exclusive commands and starting config parsing

--- btrfs-sbm.py
+++ btrfs-sbm.py
@@ -32,15 +32,29 @@ def createconfig(subvolume_path):
 # first thing, read command line options
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--hourly', action = 'store_true', default=False, help="takes hourly snapshot")
-parser.add_argument('--daily', action = 'store_true', default=False, help="takes daily snapshot")
-parser.add_argument('--list-configs', action = 'store_true',default=False, help="Prints list of configs")
-parser.add_argument('--create-config', action = 'store', help="Initializes subvolume snapshots and creates config file")
-parser.add_argument('--delete-config', action = 'store', help="removes config from table in main config file, does not delete snapshot")
+snapshot_type = parser.add_mutually_exclusive_group()
+snapshot_type.add_argument('--hourly', action = 'store_true', default=False, help="takes hourly snapshot")
+snapshot_type.add_argument('--daily', action = 'store_true', default=False, help="takes daily snapshot")
+action_group = parser.add_mutually_exclusive_group()
+action_group.add_argument('--list-configs', action = 'store_true',default=False, help="Prints list of configs")
+action_group.add_argument('--create-config', action = 'store', help="Initializes subvolume snapshots and creates config file")
+action_group.add_argument('--delete-config', action = 'store', help="removes config from table in main config file, does not delete snapshot")
 parser.add_argument('--delete-snapshots', action = 'store_true', default=False, help="combine with --delete-config to delete snapshot directory, does nothing by itself")
-parser.add_argument('--show-config', action = 'store', help="prints configuration for specific subvolume")
-parser.add_argument('--edit-config', action = 'store', help="prompts to change values configuration for specific subvolume")
+action_group.add_argument('--show-config', action = 'store', help="prints configuration for specific subvolume")
+action_group.add_argument('--edit-config', action = 'store', help="prompts to change values configuration for specific subvolume")
 parser.add_argument('--status', action = 'store_true', default=False, help="prints status")
 parser.add_argument('--sysconfig-dir', action='store', help= "changes sysconfig directory",default=os.path.join("/","etc", "conf.d"))
 parser.add_argument('--version',action='version',version=__version__ )
-parser.parse_args()
+args = parser.parse_args()
+
+main_config_file_path = os.path.join(args.sysconfig-dir,"btrfs-sbm.toml"
+
+config = toml.load(main_config_file_path)
+
+
+
+
+
+
+
+toml.dump(config, main_config_file_path)
