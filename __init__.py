149287949b1bdc02cc8579ff76b75a56eb22f89e
sww1235 <sww1235@gmail.com>
2019-02-02 21:24:50 -0700

    added __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""BTRFS Snapshot Backup Managerself.
+
+A simple collection of scripts to manage BTRFS snapshots per subvolume and
+incremental backups. Also includes uploading snapshot diffs to b2
+cloud storage.
+"""
